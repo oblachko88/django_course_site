366e0e15061e4c566148ceb1656b08cc504c2180
oblachko88 <88oblachko@gmail.com>
2024-07-14 15:50:01 +0300

    second commit

--- meetups/views.py
+++ meetups/views.py
@@ -4,6 +4,20 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, "meetups/index.html")
-
-
+    meetups = [
+        { 
+            'title': 'A first meetup', 
+            'location': 'New York', 
+            'slug': 'a-first-meetup' 
+        },
+        
+        { 
+            'title': 'A second meetup', 
+            'location': 'Paris', 
+            'slug': 'a-second-meetup' 
+        },
+    ]
+    return render(request, "meetups/index.html", {
+        'show_meetups': True,
+        'meetups': meetups
+    })
