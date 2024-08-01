34e73e9d1323a2185f7fb995d3b4c1b1134320ea
oblachko88 <88oblachko@gmail.com>
2024-08-01 12:02:58 +0300

    commit Aug 1

--- meetups/urls.py
+++ meetups/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetup/', views.index, name="all-meetups"),# our-domain.com/meetups
-    path('meetup/<slug:meetup_slug>', views.meetup_details, name="meetup-details") #our-domain.com/meetups/<dynamic-path-segment>
+    path('meetups/', views.index, name="all-meetups"),# our-domain.com/meetups
+    path('meetups/<slug:meetup_slug>', views.meetup_details, name="meetup-details") #our-domain.com/meetups/<dynamic-path-segment>
 ]
