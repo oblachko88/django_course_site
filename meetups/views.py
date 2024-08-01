0966674462dadad1714a3695d68694bd1c91e684
oblachko88 <88oblachko@gmail.com>
2024-08-01 15:18:52 +0300

    commit Aug 1

--- meetups/views.py
+++ meetups/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Meetup
 
 # Create your views here.
 
 def index(request):
-    meetups = [
-        {   'title': 'A first meetup', 
-            'location': 'New York', 
-            'slug': 'a-first-meetup' 
-        },
-        
-        { 
-            'title': 'A second meetup', 
-            'location': 'Paris', 
-            'slug': 'a-second-meetup' 
-        },
-    ]
+    meetups = Meetup.objects.all()
     return render(request, "meetups/index.html", {
-        'show_meetups': True,
         'meetups': meetups
     })
 
